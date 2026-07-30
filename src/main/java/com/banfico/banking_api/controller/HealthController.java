ef8ec705be5ce3b6feb8ebca1cb82a28cf51931f
DHANYA-P-31 <dhanyanagercoil@gmail.com>
2026-07-30 23:58:43 +0530

    Health api Added

---
+++ src/main/java/com/banfico/banking_api/controller/HealthController.java
@@ -0,0 +1,15 @@
+package com.banfico.banking_api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController
+{
+    @GetMapping("/health")
+
+    public ResponseEntity<String> heatlh(){
+        return ResponseEntity.ok("Application Running");
+    }
+}
