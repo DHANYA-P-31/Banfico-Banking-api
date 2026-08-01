2afad1c8b27522b16c7d1de8129cdfdea62db887
DHANYA-P-31 <dhanyanagercoil@gmail.com>
2026-08-01 10:07:03 +0530

    Created The repo and defined an entity for customer table

---
+++ src/main/java/com/banfico/banking_api/entity/Customer.java
@@ -0,0 +1,35 @@
+
+package com.banfico.banking_api.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String email;
+
+    @Column(name = "phone_number", nullable = false, unique = true, length = 15)
+    private String phoneNumber;
+}
---
+++ src/main/java/com/banfico/banking_api/repository/CustomerRepository.java
@@ -0,0 +1,17 @@
+package com.banfico.banking_api.repository;
+
+import com.banfico.banking_api.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByEmail(String email);
+
+    Optional<Customer> findByPhoneNumber(String phoneNumber);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByPhoneNumber(String phoneNumber);
+}
